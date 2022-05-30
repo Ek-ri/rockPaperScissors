6a4c7de00e9c41ca49cab48d82b24207e13b551d
Jesse Dodder <dodderjesse@gmail.com>
2022-05-30 09:50:48 -0600

    Create playRound function

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection = computerPlay()) {
+    const playerPick = playerSelection.toUpperCase();
+    console.log(playerPick);
+    if (playerPick === 'ROCK' && computerSelection === 'ROCK') {
+        return 'The match is a tie!';
+    } else if (playerPick === 'ROCK' && computerSelection === 'PAPER') {
+        return 'You lose! Paper beats Rock.';
+    } else if (playerPick === 'ROCK' && computerSelection === 'SCISSORS') {
+        return 'You win! Rock beats Scissors.';
+    } else if (playerPick === 'PAPER' && computerSelection === 'SCISSORS') {
+        return 'You lose! Scissors beats Paper.';
+    } else if (playerPick === 'PAPER' && computerSelection === 'ROCK') {
+        return 'You win! Paper beats Rock.';
+    } else if (playerPick === 'PAPER' && computerSelection === 'PAPER') {
+        return 'The match is a tie!';
+    } else if (playerPick === 'SCISSORS' && computerSelection === 'PAPER') {
+        return 'You win! Scissors beats Paper.';
+    } else if (playerPick === 'SCISSORS' && computerSelection === 'ROCK') {
+        return 'You lose! Rock beats Scissors.';
+    } else if (playerPick === 'SCISSORS' && computerSelection === 'SCISSORS') {
+        return 'The match is a tie!';
+    }
+}
